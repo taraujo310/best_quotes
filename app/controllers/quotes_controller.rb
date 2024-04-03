7cfcd9d58eb0f1f5b1dbf1f66d4989201444636f
Thiago Ururay <thiago@apoia.se>
2024-04-03 20:37:00 -0300

    add create action for quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,38 @@
 require "byebug"
 
 class QuotesController < Amy::Controller
+  def index
+    begin
+      @quotes = Amy::Model::FileModel.all
+      render :index
+    rescue => e
+      puts e.inspect
+    end
+  end
+
+  def create
+    @quote = Amy::Model::FileModel.create({
+      "quote" => "It is during our darkest moments that we must focus to see the light.",
+      "submitter" => "hubspot",
+      "attribution" => "Aristotle"
+    })
+
+    render :quote
+  end
+
   def a_quote
     @quote = "Do or do not. There is no try. - Yoda"
-    render :a_quote, quote: @quote
+    render :a_quote
+  end
+
+  def quote_1
+    @quote = Amy::Model::FileModel.find(1)
+    render :quote
+  end
+
+  def quote_2
+    @quote = Amy::Model::FileModel.find(2)
+    render :quote
   end
 
   def exception
