2808471c7c733dd78ad0ce335119291e757124c1
Thiago Ururay <thiago@apoia.se>
2024-04-04 07:38:31 -0300

    adds amy executable for serve and console

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,6 @@ require "rr-amy"
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "models")
 
-require "quotes_controller"
-
 module BestQuotes
   class Application < Amy::Application
     author = Amy::Author.new(name: "Thiago", email: "tufa.araujo@gmail.com", github: "https://github.com/taraujo310")
